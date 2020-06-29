f70884d085c181978b4be695c8fe6c76b637bc05
Ayush Garg <ayushgarg.ag@gmail.com>
2020-06-29 15:39:54 -0600

    Created my (Ayush) test branch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is Ayush's test branch.</Text>
+      <Text>This is Ayush's test branch</Text>
     </View>
   );
 }
