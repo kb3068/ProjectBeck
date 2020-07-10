efcf89a719daa688c2005d0bf784e160dad6c8bc
Ayush Garg <ayushgarg.ag@gmail.com>
2020-07-10 17:51:00 -0600

    Updated intro sequence

--- App.js
+++ App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import AntDesign from "react-native-vector-icons/AntDesign";
+import { StyleSheet, Text, View, TouchableWithoutFeedback, ImageBackground, Dimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/Fontisto';
+
+// Background SVG
+import BackgroundIcon from './components/BackgroundIcon';
+
+// Character SVG Icons
+import ExcitedCharacter from './components/ExcitedCharacter';
+import HappyCharacter from './components/HappyCharacter';
+import NeutralCharacter from './components/NeutralCharacter';
+import SadCharacter from './components/SadCharacter';
 
 class App extends Component {
-
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       currentId: 1
     };
+
+    // The second item in the list controls which character will show up (these strings must match with the keys in this.characterIcons)
     this.textBoxes = {
-      1: ["Hello, welcome!"],
-      2: ["I’m so excited to meet you."],
-      3: ["Please enter your name."]
+      1: ["Hello, welcome!", "excited"],
+      2: ["I’m so excited to meet you.", "happy"],
+      3: ["Please enter your name.", "neutral"]
+    }
+
+    // In order to add a character, just add the SVG in the "components" folder
+    this.characterIcons = {
+      "excited": <ExcitedCharacter style={containerStyles.character} key="excited" />,
+      "happy": <HappyCharacter style={containerStyles.character} key="happy" />,
+      "neutral": <NeutralCharacter style={containerStyles.character} key="neutral" />,
+      "sad": <SadCharacter style={containerStyles.character} key="sad" />
     }
   };
 
+  // Changes the text and image to that of the following "page" in the intro sequence
   goToNext = () => {
     if (this.state.currentId != Object.keys(this.textBoxes).length) {
       this.setState({ currentId: this.state.currentId + 1 });
     }
   };
 
+  // Changes the text and image to that of the preceding "page" in the intro sequence
   goBack = () => {
     if (this.state.currentId != 1) {
       this.setState({ currentId: this.state.currentId - 1 });
@@ -32,72 +51,91 @@ class App extends Component {
   render() {
     return (
       <TouchableWithoutFeedback onPress={this.goToNext}>
-        <View style={styles.container}>
+        <View style={containerStyles.container}>
 
-          <LinearGradient
-            colors={['#90FC8E', '#11A49B']}
-            style={{
-              position: 'absolute',
-              left: 0,
-              right: 0,
-              top: 0,
-              height: '100%',
-            }}
-          />
-          <View style={styles.aroundText}>
-            <View style={styles.leftIcon} onPress={this.goBack}>
-              <AntDesign
-                name="left"
-                color="#ccc"
-                size={45}
-                onPress={this.goBack}
-              />
+          <ImageBackground style={containerStyles.standardBackground}>
+            <BackgroundIcon style={{ zIndex: 1 }} />
+          </ImageBackground>
+
+          <View style={componentStyles.textBubble}>
+      
+            <View style={componentStyles.iconView} onPress={this.goBack}>
+              <Icon name="angle-left" size={45} color="#095266" style={componentStyles.leftIcon} onPress={this.goBack} />
             </View>
-            <Text style={styles.textBubble}>{this.textBoxes[this.state.currentId]}</Text>
+
+            <View style={componentStyles.textView}>
+              <Text style={componentStyles.text}>{this.textBoxes[this.state.currentId][0]}</Text>
+            </View>
+
           </View>
+
+          {this.characterIcons[this.textBoxes[this.state.currentId][1]]}
+
         </View>
       </TouchableWithoutFeedback>
     );
   };
 }
 
-const styles = StyleSheet.create({
+// Used to place the character in the bottom right corner, no matter the size of the screen
+const { width, height } = Dimensions.get("window");
+const screenWidth = width;
+const screenHeight = height;
+
+const containerStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    flexShrink: 1,
   },
+  standardBackground: {
+    flex: 1,
+    zIndex: 1
+  },
+  character: {
+    zIndex: 3,
+    position: 'absolute',
+    left: screenWidth - 200,
+    top: screenHeight - 400
+  }
+});
+
+const componentStyles = StyleSheet.create({
   textBubble: {
-    textAlign: 'center',
-    justifyContent: 'center',
-    fontSize: 40,
-    flexWrap: 'wrap',
-    padding: 20,
-    marginBottom: 175
-  },
-  aroundText: {
-    justifyContent: 'center',
+    zIndex: 2,
     width: 287,
     height: 525,
     backgroundColor: '#FFFFFF',
-    // shadowOffset: { width: 10, height: 10 },
-    // shadowColor: 'rgba(0, 0, 0, 0.1)',
-    overflow: 'hidden',
     borderWidth: 0,
     borderRadius: 42,
+    position: 'absolute',    
+    shadowOffset: { width: 10, height: 10 },
+    shadowRadius: 0,
+    shadowColor: '#000000',
+    shadowOpacity: .1,
   },
-  navigation: {
-    padding: 25,
-    fontSize: 20,
-    margin: 10,
-    backgroundColor: 'lightblue'
+  textView: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 100
+  },
+  text: {
+    fontSize: 42,
+    flexWrap: 'wrap',
+    textAlign: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  iconView: {
+    justifyContent: 'space-around',
+    height: 80,
   },
   leftIcon: {
-    marginTop: 10,
-    marginLeft: 10,
-    marginBottom: 'auto'
+    marginTop: 32,
+    marginLeft: 17,
   }
 });
 
